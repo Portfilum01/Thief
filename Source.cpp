a1357e35b5ae7f103e07147d6446c0812e7baefb
Portfilum01 <sabastiandust@gmail.com>
2025-02-28 11:47:47 +1030

    Added player class and int main.
    Added a player class with a power list which will possibly be there only temporarily.
    
    Added an int main so I can actually compile, doesnt do anything.

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+using namespace std;
 
 class Game 
 {
-protected:
+private:
 	int rooms[3][3] = 
 	{ 1, 2, 3,
 	  4, 5, 6,
 	  7, 8, 9, 
 	};
+	
+	/*playerCharacter*;*/
 
 public:
 	Game();
@@ -16,9 +19,20 @@ public:
 
 };
 
+class Player
+{
+private:
+	string powerList[5] = { "Levitate", "Thermal Transfer", "Surge", "Aquagenesis", "Expel" };
+};
+
 class Item 
 {
 public:
-	std::string description;
+	string description;
 	void use();
 };
+
+int main()
+{
+	return 0;
+}
